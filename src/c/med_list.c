8e2dc2a6993e6f1837d02d5b82f087906e5d4afe
Britt Piehler <brittp@gmail.com>
2026-04-12 17:28:29 -0700

    Add TEST_MODE to med_list.c for emulator testing

--- src/c/med_list.c
+++ src/c/med_list.c
@@ -1,5 +1,8 @@
 #include "med_list.h"
 #include <pebble.h>
+#include <string.h>
+
+#define TEST_MODE 1
 
 // Persist key layout:
 //   0        = uint8  med count
@@ -13,6 +16,53 @@ static MedEntry  s_meds[MED_MAX];
 static uint8_t   s_count = 0;
 static AppSettings s_settings = { .snoozeMins = 15, .privacyMode = false };
 
+// ---------------------------------------------------------------------------
+// Test Data
+// ---------------------------------------------------------------------------
+
+#if TEST_MODE
+static void load_test_data(void) {
+    s_count = 3;
+    
+    // 1. Fixed time (Self)
+    strncpy(s_meds[0].taker, "Self", sizeof(s_meds[0].taker));
+    strncpy(s_meds[0].name, "Multivitamin", sizeof(s_meds[0].name));
+    strncpy(s_meds[0].dose, "1 tablet", sizeof(s_meds[0].dose));
+    s_meds[0].scheduleType = SCHEDULE_FIXED;
+    s_meds[0].timeCount = 1;
+    s_meds[0].times[0] = (TimeEntry){ .h = 8, .m = 0 };
+    s_meds[0].shape = SHAPE_ROUND;
+    s_meds[0].color = GColorYellow;
+
+    // 2. Interval (Murray the dog)
+    strncpy(s_meds[1].taker, "Murray", sizeof(s_meds[1].taker));
+    strncpy(s_meds[1].name, "Apoquel", sizeof(s_meds[1].name));
+    strncpy(s_meds[1].dose, "16mg", sizeof(s_meds[1].dose));
+    s_meds[1].scheduleType = SCHEDULE_INTERVAL;
+    s_meds[1].intervalHours = 12;
+    s_meds[1].startHour = 9;
+    s_meds[1].startMinute = 0;
+    s_meds[1].lastTakenTs = 0; // Will use start time
+    s_meds[1].shape = SHAPE_OVAL;
+    s_meds[1].color = GColorWhite;
+
+    // 3. Multiple times (Self)
+    strncpy(s_meds[2].taker, "Self", sizeof(s_meds[2].taker));
+    strncpy(s_meds[2].name, "Ibuprofen", sizeof(s_meds[2].name));
+    strncpy(s_meds[2].dose, "400mg", sizeof(s_meds[2].dose));
+    s_meds[2].scheduleType = SCHEDULE_FIXED;
+    s_meds[2].timeCount = 2;
+    s_meds[2].times[0] = (TimeEntry){ .h = 10, .m = 0 };
+    s_meds[2].times[1] = (TimeEntry){ .h = 22, .m = 0 };
+    s_meds[2].shape = SHAPE_OBLONG;
+    s_meds[2].color = GColorRed;
+}
+#endif
+
+// ---------------------------------------------------------------------------
+// Persistence
+// ---------------------------------------------------------------------------
+
 void med_list_init(void) {
     if (persist_exists(PERSIST_KEY_MED_COUNT)) {
         s_count = (uint8_t)persist_read_int(PERSIST_KEY_MED_COUNT);
@@ -20,7 +70,13 @@ void med_list_init(void) {
         for (uint8_t i = 0; i < s_count; i++) {
             persist_read_data(PERSIST_KEY_MED_BASE + i, &s_meds[i], sizeof(MedEntry));
         }
+    } 
+#if TEST_MODE
+    else {
+        load_test_data();
     }
+#endif
+
     if (persist_exists(PERSIST_KEY_SETTINGS)) {
         persist_read_data(PERSIST_KEY_SETTINGS, &s_settings, sizeof(AppSettings));
     }
@@ -48,8 +104,9 @@ void med_list_set_count(uint8_t count) {
 }
 
 void med_list_set(uint8_t index, const MedEntry *entry) {
-    if (index >= MED_MAX || !entry) return;
-    s_meds[index] = *entry;
+    if (index < MED_MAX) {
+        memcpy(&s_meds[index], entry, sizeof(MedEntry));
+    }
 }
 
 AppSettings *med_list_get_settings(void) {
@@ -61,37 +118,38 @@ void med_list_save_settings(void) {
 }
 
 time_t med_list_next_dose_time(const MedEntry *med, time_t after) {
+    struct tm t_after = *localtime(&after);
+
     if (med->scheduleType == SCHEDULE_FIXED) {
-        struct tm *t = localtime(&after);
-        time_t midnight = after - ((time_t)t->tm_hour * 3600
-                                 + (time_t)t->tm_min  * 60
-                                 + (time_t)t->tm_sec);
         time_t best = 0;
-        for (uint8_t day = 0; day <= 1; day++) {
-            for (uint8_t i = 0; i < med->timeCount; i++) {
-                time_t ts = midnight
-                          + (time_t)day * 86400
-                          + (time_t)med->times[i].h * 3600
-                          + (time_t)med->times[i].m * 60;
-                if (ts > after && (best == 0 || ts < best)) best = ts;
-            }
+        // Check all times for the earliest one today/tomorrow
+        for (int i = 0; i < med->timeCount; i++) {
+            struct tm t = t_after;
+            t.tm_hour = med->times[i].h;
+            t.tm_min  = med->times[i].m;
+            t.tm_sec  = 0;
+            time_t occ = mktime(&t);
+            if (occ <= after) occ += 24 * 3600; // Tomorrow
+            if (best == 0 || occ < best) best = occ;
         }
-        return best ? best : after + 86400;
+        return best;
     } else {
-        if (med->lastTakenTs > 0) {
-            time_t next = (time_t)med->lastTakenTs + (time_t)med->intervalHours * 3600;
-            while (next <= after) next += (time_t)med->intervalHours * 3600;
-            return next;
+        // Interval: lastTakenTs + hours (fallback to start time if lastTakenTs is 0)
+        if (med->lastTakenTs == 0) {
+            struct tm t = t_after;
+            t.tm_hour = med->startHour;
+            t.tm_min  = med->startMinute;
+            t.tm_sec  = 0;
+            time_t occ = mktime(&t);
+            if (occ <= after) {
+                // How many intervals have passed since start time today?
+                int seconds_since = (int)(after - occ);
+                int intervals = (seconds_since / (med->intervalHours * 3600)) + 1;
+                occ += intervals * (med->intervalHours * 3600);
+            }
+            return occ;
         } else {
-            struct tm *t = localtime(&after);
-            time_t midnight = after - ((time_t)t->tm_hour * 3600
-                                     + (time_t)t->tm_min  * 60
-                                     + (time_t)t->tm_sec);
-            time_t start = midnight
-                         + (time_t)med->startHour   * 3600
-                         + (time_t)med->startMinute * 60;
-            while (start <= after) start += (time_t)med->intervalHours * 3600;
-            return start;
+            return (time_t)med->lastTakenTs + (med->intervalHours * 3600);
         }
     }
 }
